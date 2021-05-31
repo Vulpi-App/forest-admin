39849105f056cc04a50a1e96b14ba8cec713a782
Manon Boiteau <manonboiteau@MacBook-Pro-de-Manon.local>
2021-05-31 15:29:35 +0200

    tests

--- api/products.js
+++ api/products.js
@@ -2,15 +2,32 @@ const express = require("express");
 // const formidable = require("express-formidable");
 const router = express.Router();
 
+// Import Middleware
+const isAuthenticated = require("./middleware/isAuthenticated");
+
 const { products } = require("../models");
 // console.log(products);
 
-// Pas de app.use("formidable") si cette syntaxe : router.get("/api/users", formidable(), async (req, res) => {
-
-router.get("/products", async (req, res) => {
+router.get("/products", isAuthenticated, async (req, res) => {
   try {
     const productsList = await products.find();
     res.status(200).json(productsList);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/products/:id", isAuthenticated, async (req, res) => {
+  try {
+    console.log(req.params.id);
+
+    const productToFind = await products.findById(req.params.id);
+
+    if (productToFind) {
+      res.status(200).json({ productToFind });
+    } else {
+      res.status(400).json({ message: "This product does not exist 😬" });
+    }
   } catch (error) {
     console.log(error);
   }
